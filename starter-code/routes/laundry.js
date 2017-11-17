64831189b5c9294daf0f33613799a79746f313ef
sdediego <sergio.dediego@outlook.com>
2017-11-17 12:27:23 +0100

    Iteracion 7

--- starter-code/routes/laundry.js
+++ starter-code/routes/laundry.js
@@ -14,7 +14,29 @@ router.use((req, res, next) => {
 });
 
 router.get('/dashboard', (req, res, next) => {
-  res.render('laundry/dashboard');
+  let query;
+
+  if (req.session.currentUser.isLaunderer) {
+    query = { launderer: req.session.currentUser._id };
+  } else {
+    query = { user: req.session.currentUser._id };
+  }
+
+  LaundryPickup
+    .find(query)
+    .populate('user', 'name')
+    .populate('launderer', 'name')
+    .sort('pickupDate')
+    .exec((err, pickupDocs) => {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      res.render('laundry/dashboard', {
+        pickups: pickupDocs
+      });
+    });
 });
 
 router.get('/launderers', (req, res, next) => {
@@ -82,6 +104,8 @@ router.post('/laundry-pickups', (req, res, next) => {
     res.redirect('/laundry/dashboard');
   });
 });
+
+
 
 
 module.exports = router;
